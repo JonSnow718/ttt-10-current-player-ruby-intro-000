e6c273a038bbbadc353f91e277f933431ab0723a
Jason Calvo <jason.calvo7@gmail.com>
2020-05-06 19:33:19 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0
+  #move_count = []
+  turn.each.do |board|
+    if unit == "X" || unit == "O"
+      move_count.push(1)
+    end
+  move_count.length.to_i
+end
+
+def current_player(board)
+  num == turn_count(board)
+  if num % 2 == 0 
+    return "X"
+  else 
+    return "O"
+  end
+end
+
+    
